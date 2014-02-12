0a5411d0fd6ad2370cf3bc108f0bf37b864917f2
Andy Royle <ajroyle@gmail.com>
2014-02-12 17:21:02 +0000

    hook up gruntfile with tests

---
+++ Gruntfile.js
@@ -0,0 +1,40 @@
+module.exports = function(grunt) {
+    'use strict';
+
+    // Project configuration.
+    grunt.initConfig({
+        jshint: {
+            all: [
+                'Gruntfile.js',
+                'tasks/*.js'
+            ],
+            options: {
+                jshintrc: '.jshintrc'
+            }
+        },
+        mochaTest:{
+            options: {
+                reporter: 'spec'
+            },
+            tests:{
+                src: ['tests/get-last-pinned-tests.js']
+            }
+        },
+        'get-last-pinned-buildnumber': {
+            'test': {
+                options: {
+                    url: 'http://127.0.0.1:8888',
+                    buildTypeId: 123
+                }
+            }
+        }
+    });
+
+    // These plugins provide necessary tasks.
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-mocha-test');
+    grunt.registerTask('test', ['jshint', 'start-server', 'get-last-pinned-buildnumber:test', 'mochaTest']);
+    grunt.registerTask('default', ['test']);
+    grunt.loadTasks('tasks');
+    grunt.loadTasks('tests/tasks');
+};
--- tasks/get-last-pinned.js
+++ tasks/get-last-pinned.js
@@ -6,8 +6,6 @@ var request = require('request'),
 
 module.exports = function(grunt){
 
-    var options = this.options({});
-
     var saveBuildInfo = function(buildInfo, done){
         if (buildInfo.build.length > 0){
             grunt.verbose.writeln('Build number is ' + buildInfo.build[0].number);
@@ -26,24 +24,23 @@ module.exports = function(grunt){
         }
     };
 
-    grunt.registerTask('set-buildnumber', 'Reads the last pinned build number from file and populates property', function(){
+    grunt.registerMultiTask('set-buildnumber', 'Reads the last pinned build number from file and populates property', function(){
         var buildInfo = JSON.parse(fs.readFileSync(outputFile));
         var buildNumber = buildInfo.build[0].number;
         grunt.log.debug('Build number in local file is ' + buildNumber);
         grunt.option("buildNumber", buildNumber);
     });
 
-    grunt.registerTask('get-last-pinned-buildnumber', 'Uses the TeamCity API to ask for a specific projects last pinned build number', function(){
-
+    grunt.registerMultiTask('get-last-pinned-buildnumber', 'Uses the TeamCity API to ask for a specific projects last pinned build number', function(){
+        var options = this.options({});
+        grunt.verbose.writeflags(options);
         grunt.verbose.writeln('Getting last pinned build...');
-        grunt.config.requires(options.url);
-        grunt.config.requires(options.buildTypeId);
 
         var done = this.async();
         var url = options.url;
         var buildType = options.buildTypeId;
 
-        url = url + '/guestAuth/app/rest/builds/?locator=buildType:' + buildType + ',status:SUCCESS,pinned:true,count:1'
+        url = url + '/guestAuth/app/rest/builds/?locator=buildType:' + buildType + ',status:SUCCESS,pinned:true,count:1';
         grunt.log.debug('Making request to teamcity ' + url);
 
         request({
@@ -56,7 +53,7 @@ module.exports = function(grunt){
             if (error){
                 grunt.fail.fatal(error);
             }
-            if (response.statusCode == 200){
+            if (response.statusCode === 200){
                 grunt.verbose.debug('The response from TeamCity request: ' + body);
                 saveBuildInfo(JSON.parse(body), done);
             }
